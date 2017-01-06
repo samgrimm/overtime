7974e1e6ff1b68107e6cf84db71979874e102b89
samgrimm <samantha.grimm@gmail.com>
2017-01-06 15:37:14 -0600

    implement creation process for posts and refactor

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
 
   def index
   end
@@ -10,19 +11,23 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     if @post.save
-      redirect_to @post
+      redirect_to @post, notice: "Your post was created successfully"
     else
       render 'new'
     end
   end
 
   def show
-    @post = Post.find(params[:id])
+
   end
 
   private
 
   def post_params
     params.require(:post).permit(:date, :rationale)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end

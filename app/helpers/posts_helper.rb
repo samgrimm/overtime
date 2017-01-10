39a1dfb55f771e7c6d9665b0e0f990e29030ebf7
samgrimm <samantha.grimm@gmail.com>
2017-01-09 20:25:05 -0600

    add labels with status values on index for posts

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,18 @@
 module PostsHelper
+  def status_label status
+    status_span_generator status
+  end
+
+  private
+
+  def status_span_generator status
+    case status
+    when 'submitted'
+      content_tag(:td, status.titleize, class: 'table-info')
+    when 'approved'
+      content_tag(:td, status.titleize, class: 'table-success')
+    when 'rejectec'
+      content_tag(:td, status.titleize, class: 'table-danger')
+    end
+  end
 end

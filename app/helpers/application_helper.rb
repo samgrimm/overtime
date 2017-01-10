dc5148abc2a28d3d60e44ee59ce65ef3f5447eb2
samgrimm <samantha.grimm@gmail.com>
2017-01-10 16:14:02 -0600

    integrated application helper for status labels and created enums for audit logs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,26 @@ module ApplicationHelper
   def active?(path)
     "active" if current_page?(path)
   end
+
+  def status_label status
+    status_span_generator status
+  end
+
+  private
+
+  def status_span_generator status
+    case status
+      when 'submitted'
+        content_tag(:td, status.titleize, class: 'table-info')
+      when 'approved'
+        content_tag(:td, status.titleize, class: 'table-success')
+      when 'rejected'
+        content_tag(:td, status.titleize, class: 'table-danger')
+      when 'pending'
+        content_tag(:td, status.titleize, class: 'table-danger')
+      when 'confirmed'
+        content_tag(:td, status.titleize, class: 'table-success')
+    end
+  end
+  
 end

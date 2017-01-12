a70f94e750ef07e57d08d34e8b959762cc6c259b
samgrimm <samantha.grimm@gmail.com>
2017-01-12 13:44:22 -0600

    added a fix to seeds so it will work around the callback

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,13 +15,11 @@ puts "1 Admin User created"
                     )
 puts "1 User created"
 
+AuditLog.create(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+
 100.times do |post|
   Post.create(date: Date.today, rationale: "How long before you can make the jump to light speed? It'll take a few moments to get the coordinates from the navi-computer. Are you kidding? At the rate they're gaining... Traveling through hyperspace isn't like dusting crops, boy! Without precise calculations we could fly right through a star or bounce too close to a supernova and that'd end your trip real quick, wouldn't it? What's that flashing? We're losing our deflector shield. Go strap yourself in, I'm going to make the jump to light speed.", user_id: @user.id, overtime_request:2.5)
 end
 puts "100 Posts have been created"
-
-100.times do |audit_log|
-  AuditLog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 6.days))
-end
-
-puts "100 Audit Logs have been created"

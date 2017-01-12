5f7d2058d74398f46c47966b69e890f36be0fb66
samgrimm <samantha.grimm@gmail.com>
2017-01-12 06:57:54 -0600

    added the logic to the buttons to confirm no overtime

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -9,4 +9,13 @@ describe 'Homepage' do
     click_on("approve_#{post.id}")
     expect(post.reload.status).to eq("approved")
   end
+
+  it "allows the user to change the audit log status from the home page" do
+    user = FactoryGirl.create(:user)
+    audit_log = FactoryGirl.create(:audit_log, user_id: user.id)
+    login_as(user, :scope => :user)
+    visit root_path
+    click_on("confirm_#{audit_log.id}")
+    expect(audit_log.reload.status).to eq("confirmed")
+  end
 end

2c26bae5066e3b802cb95c167e518616338feba3
samgrimm <samantha.grimm@gmail.com>
2017-01-17 07:26:42 -0600

    added email setup to production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,6 +58,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "Overtime_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { :host => 'sam-overtime.herokuapp.com'}
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false

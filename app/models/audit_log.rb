7744c4e482482664c84e7d0908ac138c9296b621
samgrimm <samantha.grimm@gmail.com>
2017-01-29 12:48:29 -0600

    added callback to create audit log end date

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -3,11 +3,15 @@ class AuditLog < ApplicationRecord
   belongs_to :user
 
   validates_presence_of :user_id, :status, :start_date
-
+  before_update :set_end_date, if: :confirmed?
 
   after_initialize :set_defaults
 
   private
+
+  def set_end_date
+    self.end_date = Date.today
+  end
 
   def set_defaults
     self.start_date ||= Date.today - 6.days
